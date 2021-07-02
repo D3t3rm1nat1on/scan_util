270b5a5a9c0007b1f719eda891702fb3f150fad8
andrey <bro.chek@mail.ru>
2021-07-02 17:47:21 +0300

    init

---
+++ tests/js_error.js
@@ -0,0 +1,2 @@
+std::string js_string = "<script>evil_scriasdasdasdasdasdadasdpt()</script>";
+BUT U CANT READ THIS
---
+++ tests/js_sus.js
@@ -0,0 +1,3 @@
+bruh
+std::string js_string = "<script>evil_script()</script>";
+oh my god
