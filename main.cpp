367a2a5ffdb08bf139486dae3d55acd5372aa1fe
andrey <bro.chek@mail.ru>
2021-07-04 12:37:44 +0300

    refactor + directory permission error check

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
 #include <filesystem>
 #include <string>
 #include <fstream>
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <condition_variable>
+#include <atomic>
+#include <iostream>
 
-#define THREAD_COUNT 2
+#define THREAD_COUNT 3
 #define SUS_TYPE_COUNT 3
-static std::atomic<size_t> error_counter{};
-static std::atomic<size_t> sus_counters[SUS_TYPE_COUNT]{};
-// 0 - js
-// 1 - unix
-// 2 - macOS
 
-static std::atomic<int> thread_counter = THREAD_COUNT;
-static std::mutex m{};
-static std::condition_variable cv{};
-
-enum sus_types
+enum suspicious_types_enum
 {
     NONE = -1,
     js = 0,
@@ -26,42 +19,58 @@ enum sus_types
     maxOS = 2
 };
 
-sus_types is_sus(const std::string &str, const std::string &ext);
+suspicious_types_enum suspicion_check(const std::string &str, const std::string &ext);
+void check_file(const std::filesystem::path &path);
 
-void threadFunction(const std::filesystem::path &path);
+static std::atomic<size_t> error_counter{};
+static std::atomic<size_t> suspicious_files_counters[SUS_TYPE_COUNT]{};
+// 0 - js
+// 1 - unix
+// 2 - macOS
+static std::atomic<int> available_threads_counter = THREAD_COUNT;
+static std::condition_variable cv{};
 
 int main(int argc, char *argv[])
 {
-    std::chrono::time_point start_time{std::chrono::high_resolution_clock::now()};
+    auto start_time{std::chrono::high_resolution_clock::now()};
+    size_t file_counter{};
+    std::mutex m{};
+    std::vector<std::thread> thread_vector{};
+    std::unique_lock<std::mutex> lock{m};
 
     if (argc != 2)
     {
-        std::cerr << "wrong param count, expected path only\n";
+        fprintf(stderr, "wrong param count, expected path only\n");
         return -1;
     }
 
     std::filesystem::path path{argv[1]};
     if (!std::filesystem::exists(path))
     {
-        std::cerr << "this catalog doesn't exists\n";
+        fprintf(stderr, "directory doesn't exist: %s\n", path.string().c_str());
         return -1;
     }
 
-    size_t file_counter{};
-    std::vector<std::thread> vec{};
-    std::unique_lock<std::mutex> lock{m/*, std::defer_lock*/};
+    {
+        // Hello from RAII
+        std::ifstream dir{path};
+        if (!dir.is_open())
+        {
+            fprintf(stderr, "operation not permitted for directory: %s\n", path.string().c_str());
+            return 6;
+        }
+    }
+
     for (const auto &iter : std::filesystem::directory_iterator{path})
     {
         cv.wait(lock, [&] {
-            std::cout << "there " << thread_counter << " threads ready" << std::endl;
-            return thread_counter > 0;
+            return available_threads_counter > 0;
         });
         ++file_counter;
-        --thread_counter;
-        vec.emplace_back(std::thread(threadFunction, iter.path()));
+        --available_threads_counter;
+        thread_vector.emplace_back(std::thread(check_file, iter.path()));
     }
-//    std::this_thread::sleep_for(std::chrono::seconds(5));
-    for (auto &thr : vec)
+    for (auto &thr : thread_vector)
         thr.join();
 
     auto duration{std::chrono::high_resolution_clock::now() - start_time};
@@ -71,60 +80,56 @@ int main(int argc, char *argv[])
 
     printf("========== Scan result ==========\n");
     printf("Processed files: %zu\n", file_counter);
-    printf("JS detects: %zu\n", sus_counters[js].load());
-    printf("Unix detects: %zu\n", sus_counters[unix].load());
-    printf("macOS detects: %zu\n", sus_counters[maxOS].load());
+    printf("JS detects: %zu\n", suspicious_files_counters[js].load());
+    printf("Unix detects: %zu\n", suspicious_files_counters[unix].load());
+    printf("macOS detects: %zu\n", suspicious_files_counters[maxOS].load());
     printf("Errors: %zu\n", error_counter.load());
-    printf("Exection time: %02llds:%02lldms:%02lldus\n", duration_s, duration_ms, duration_us);
-    std::cout << "=================================\n" << std::endl;
+    printf("Execution time: %02llds:%02lldms:%02lldus\n", duration_s, duration_ms, duration_us);
+    printf("=================================\n");
 }
 
-
-sus_types is_sus(const std::string &str, const std::string &ext)
+suspicious_types_enum suspicion_check(const std::string &str, const std::string &ext)
 {
     static const std::string js_string = "<script>evil_script()</script>";
     static const std::string unix_string = "rm -rf ~/Document";
     static const std::string macOS_string = "system(\"launchctl load /Library/LaunchAgents/com.malware.agent\")";
 
     if (ext == ".js" && str.find(js_string) != -1)
-        return sus_types::js;
+        return suspicious_types_enum::js;
 
     if (str.find(unix_string) != -1)
-        return sus_types::unix;
+        return suspicious_types_enum::unix;
 
     if (str.find(macOS_string) != -1)
-        return sus_types::maxOS;
+        return suspicious_types_enum::maxOS;
 
     return NONE;
 }
 
-void threadFunction(const std::filesystem::path &path)
+void check_file(const std::filesystem::path &path)
 {
-    std::cerr << "thread started: " << std::this_thread::get_id() << std::endl;
-
     std::ifstream file{path};
 
     if (!file.is_open())
     {
         ++error_counter;
-        ++thread_counter;
+        ++available_threads_counter;
+        cv.notify_all();
         return;
     }
 
     std::string line;
-    auto extension = path.extension().string();
+    std::string extension = path.extension().string();
     while (getline(file, line))
     {
-        auto result = is_sus(line, extension);
+        auto result = suspicion_check(line, extension);
         if (result == NONE)
             continue;
 
-        ++sus_counters[result];
+        ++suspicious_files_counters[result];
         break;
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    ++thread_counter;
-    std::cout << "thread ended: " << std::this_thread::get_id() << ", now " << thread_counter << " threads available" << std::endl;
+    ++available_threads_counter;
     cv.notify_all();
 }
